1f16ba981c0c1ee0f037cf4a616ee3c28ddce701
Pavel Alimov <pashaalimov@gmail.com>
2024-11-22 18:41:24 +0300

    Add wave-gauss single sample

--- visualization.py
+++ visualization.py
@@ -1,61 +1,75 @@
 import random
-
 import netCDF4
 import torch
-
 from CNO2d_time_dependent_and_foundation_model.test_and_fine_tune_utils.fine_tune_utils import (
     load_model,
 )
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def create_frames(model, dataset, gif_name="cno_gauss.gif"):
+    fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
+
+    start_time = 0
+    end_time = 15
+    sample_num = random.randint(0, 10000)
+    inputs_function = torch.tensor(dataset["solution"][sample_num, start_time]).reshape(
+        1, 128, 128
+    )
+    inputs_condition = torch.tensor(dataset["c"][sample_num]).reshape(1, 128, 128)
+
+    def create_frame(t):
+        time = (t - start_time) / end_time
+        time_delta = torch.ones(1, 128, 128).type(torch.float32) * time
+        inputs = torch.cat(
+            [inputs_function, inputs_condition, time_delta], dim=0
+        ).unsqueeze(dim=0)
+
+        labels = torch.tensor(dataset["solution"][sample_num, t]).reshape(1, 128, 128)
+        labels = torch.cat([labels, inputs_condition], dim=0).unsqueeze(dim=0)
+
+        predicted = model.forward(inputs, torch.tensor(t)).detach()
+        all_loss = torch.nn.L1Loss()(predicted, labels)
+        func_loss = torch.nn.L1Loss()(predicted[0][0], labels[0][0])
+        ic_loss = torch.nn.L1Loss()(predicted[0][1], labels[0][1])
+
+        plt.cla()
+        ax[0, 0].imshow(inputs[0][0])
+        ax[0, 0].set_title(f"Input at time {start_time}")
+        ax[1, 0].imshow(inputs_condition[0])
+        ax[1, 0].set_title(f"Initial conditions")
+
+        ax[0, 1].imshow(predicted[0][0])
+        ax[0, 1].set_title(f"Predicted at time {t}")
+        ax[1, 1].imshow(predicted[0][1])
+        ax[1, 1].set_title(f"Predicted conditions")
+
+        ax[0, 2].imshow(labels[0][0])
+        ax[0, 2].set_title(f"True at time {t}")
+        ax[1, 2].imshow(labels[0][1])
+        ax[1, 2].set_title(f"Initial conditions")
+
+        fig.suptitle(
+            f"All lose = {all_loss:.3f}, function loss = {func_loss:.3f}, initial condition loss = {ic_loss:.3f}"
+        )
+        return ax[0, 0], ax[1, 0], ax[0, 1], ax[1, 1], ax[0, 2], ax[1, 2]
+
+    gif = animation.FuncAnimation(fig, create_frame, frames=15)
+    gif.save(gif_name, dpi=300, writer="pillow", fps=3)
+
 
 which_example = "wave_gauss"
-model = load_model(
+cno, loader_dict = load_model(
     folder=f"TrainedModels/Time_CNO_{which_example}_1",
     which_example=which_example,
     in_dim=3,
     out_dim=2,
     label="794",
 )
-cno = model[0]
-print(model[1].keys())
 wave_gauss_nc = netCDF4.Dataset("nc_data/res/Wave-Gauss.nc")
-# initial_condition = netCDF4.Dataset("nc_data/c_0.nc")
+wave_gauss_ic_nc = netCDF4.Dataset("nc_data/c_0.nc")
 print(wave_gauss_nc)
-print(wave_gauss_nc.variables.keys())
+print(wave_gauss_ic_nc)
 
-sample_num = random.randint(0, 10000)
-time_steps = 15
-t1 = random.randint(0, 15)
-t2 = random.randint(t1, 15)
-time = (t2 - t1) / time_steps
-
-inputs = torch.tensor(wave_gauss_nc["solution"][sample_num, t1]).reshape(1, 128, 128)
-inputs_condition = torch.tensor(wave_gauss_nc["c"][sample_num]).reshape(1, 128, 128)
-time_delta = torch.ones(1, 128, 128).type(torch.float32) * time
-
-inputs = torch.cat([inputs, inputs_condition, time_delta], dim=0).unsqueeze(dim=0)
-
-labels = torch.tensor(wave_gauss_nc["solution"][sample_num, t2]).reshape(1, 128, 128)
-labels = torch.cat([labels, inputs_condition], dim=0).unsqueeze(dim=0)
-
-prediction: torch.Tensor = cno.forward(inputs, torch.tensor(4))
-prediction = prediction.detach()
-
-fig, ax = plt.subplots(nrows=2, ncols=3)
-
-ax[0, 0].imshow(inputs[0][0])
-ax[0, 0].set_title(f"Input at time {t1}")
-ax[1, 0].imshow(inputs_condition[0])
-ax[1, 0].set_title(f"Initial conditions")
-
-ax[0, 1].imshow(prediction[0][0])
-ax[0, 1].set_title(f"Predicted at time {t2}")
-ax[1, 1].imshow(prediction[0][1])
-ax[1, 1].set_title(f"Predicted conditions")
-
-ax[0, 2].imshow(labels[0][0])
-ax[0, 2].set_title(f"True at time {t2}")
-ax[1, 2].imshow(labels[0][1])
-ax[1, 2].set_title(f"Initial conditions")
-
-plt.show()
+create_frames(cno, wave_gauss_nc)
---
+++ visualization_tuned.py
@@ -0,0 +1,27 @@
+import random
+import netCDF4
+import torch
+import matplotlib.pyplot as plt
+from test_and_fine_tune_utils.test_utils import _load_dict, _initialize_model
+from visualization import create_frames
+
+
+which_example = "wave_gauss"
+folder = f"TrainedModels/fine_tuned"
+loader_dict = _load_dict(
+    files=[folder + "/training_properties.txt", folder + "/net_architecture.txt"],
+    which_example=which_example,
+)
+print(loader_dict)
+cno = _initialize_model(
+    loader_dict,
+    diff_embedding=True,
+    old_in_dim=5,
+    new_in_dim=3,
+    new_out_dim=2,
+)
+wave_gauss_nc = netCDF4.Dataset("nc_data/res/Wave-Gauss.nc")
+print(wave_gauss_nc)
+print(wave_gauss_nc.variables.keys())
+
+create_frames(cno, wave_gauss_nc, "cno-tuned-gauss.gif")
