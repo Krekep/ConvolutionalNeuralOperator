ef9d90e5391edf721fea7879bba356f65cb0167c
Pavel Alimov <pashaalimov@gmail.com>
2024-12-02 13:29:17 +0300

    Add piezo conductivity to DatasetLoaders

--- CNO2d_time_dependent_and_foundation_model/DataLoaders/CNO_TimeLoaders.py
+++ CNO2d_time_dependent_and_foundation_model/DataLoaders/CNO_TimeLoaders.py
@@ -1768,6 +1768,84 @@ class WaveGaussians(BaseTimeDataset):
 
 
 # --------------------------------------------------------
+# Piezo conductivity:
+# --------------------------------------------------------
+
+
+class PiezoConductivity(BaseTimeDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        assert self.max_num_time_steps * self.time_step_size <= 21
+
+        self.N_max = 4096
+        self.N_val = 60
+        self.N_test = 240
+        self.resolution = 128
+
+        data_path = self.data_path + "/piezo_conductivity.nc"
+        self.reader = h5py.File(data_path, "r")
+
+        self.constants = {
+            # "mean": 0.0334376316,
+            # "std": 0.1171879068,
+            # "mean_c": 2618.4593933,
+            # "std_c": 601.51658913,
+            "time": 21.0,
+        }
+
+        self.input_dim = 2
+        self.label_description = "[u],[c]"
+
+        self.post_init()
+
+    def __getitem__(self, idx):
+        i = idx // self.multiplier
+        _idx = idx - i * self.multiplier
+
+        if self.fix_input_to_time_step is None:
+            t1, t2 = self.time_indices[_idx]
+            assert t2 >= t1
+            t = t2 - t1
+        else:
+            t1 = self.fix_input_to_time_step
+            t2 = self.time_step_size * (_idx + 1)
+            t = t2 - t1
+        time = t / self.constants["time"]
+
+        inputs = (
+            torch.from_numpy(self.reader["solution"][i + self.start, t1])
+            .type(torch.float32)
+            .reshape(1, self.resolution, self.resolution)
+        )
+        inputs_c = (
+            torch.from_numpy(self.reader["c"][i + self.start])
+            .type(torch.float32)
+            .reshape(1, self.resolution, self.resolution)
+        )
+        labels = (
+            torch.from_numpy(self.reader["solution"][i + self.start, t2])
+            .type(torch.float32)
+            .reshape(1, self.resolution, self.resolution)
+        )
+
+        # inputs = (inputs - self.constants["mean"]) / self.constants["std"]
+        # inputs_c = (inputs_c - self.constants["mean_c"]) / self.constants["std_c"]
+        # labels = (labels - self.constants["mean"]) / self.constants["std"]
+
+        inputs = torch.cat([inputs, inputs_c], dim=0)
+        labels = torch.cat([labels, inputs_c], dim=0)
+
+        if self.time_input:
+            inputs_t = (
+                torch.ones(1, self.resolution, self.resolution).type(torch.float32)
+                * time
+            )
+            inputs = torch.cat((inputs, inputs_t), 0)
+
+        return time, inputs, labels
+
+
+# --------------------------------------------------------
 # Kolmogorov:
 # --------------------------------------------------------
 
