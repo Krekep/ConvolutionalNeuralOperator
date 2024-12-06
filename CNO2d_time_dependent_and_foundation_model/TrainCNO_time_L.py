a17cff617daa6204c15e94938de27959147bbba9
Pavel Alimov <pashaalimov@gmail.com>
2024-12-06 12:38:15 +0300

    Update train config

--- CNO2d_time_dependent_and_foundation_model/TrainCNO_time_L.py
+++ CNO2d_time_dependent_and_foundation_model/TrainCNO_time_L.py
@@ -25,11 +25,11 @@ if len(sys.argv) <= 4:
         "weight_decay": 1e-6,
         "scheduler_step": 1,
         "scheduler_gamma": 0.9,
-        "epochs": 200,
+        "epochs": 250,
         "batch_size": 32,
-        "time_steps": 7,  # How many time steps to select?
+        "time_steps": 10,  # How many time steps to select?
         "dt": 1,  # What is the time step? (1 means include entire traj, 2 means taking every other step, etc.
-        "training_samples": 32,  # How many training samples?
+        "training_samples": 64,  # How many training samples?
         "time_input": 1,  # Should we include time in the input channels?
         "allowed": "all",  # All2ALL (train) - all , or One2All (train) - one2all, AR training - one
         "cluster": True,  # Something internal (don't bother)
@@ -48,7 +48,7 @@ if len(sys.argv) <= 4:
         "is_att": False,  # Should we use attention in the bottleneck? You could add it!
         "patch_size": 1,  # ViT parameters, if is_att == True
         "dim_multiplier": 1,
-        "depth": 2,
+        "depth": 3,
         "heads": 2,
         "dim_head_multiplier": 0.5,
         "mlp_dim_multiplier": 1.0,
